4fc56abec2faeac7e49659415625081b49bbce35
miguel <miguel@localhost.localdomain>
2023-06-12 21:14:01 +0200

    .config files updated

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -13,13 +13,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 ## Left bar
 polybar log -c ~/.config/polybar/current.ini &
 polybar secondary -c ~/.config/polybar/current.ini &
-polybar terciary -c ~/.config/polybar/current.ini &
-polybar quaternary -c ~/.config/polybar/current.ini &
-polybar target -c ~/.config/polybar/current.ini &
+polybar ethernet_bar -c ~/.config/polybar/current.ini &
+polybar vpn_bar -c ~/.config/polybar/current.ini &
 
 ## Right bar
-polybar top -c ~/.config/polybar/current.ini &
+# polybar top -c ~/.config/polybar/current.ini &
 polybar primary -c ~/.config/polybar/current.ini &
+polybar victim_target -c ~/.config/polybar/current.ini &
 
 ## Center bar
 polybar primary -c ~/.config/polybar/workspace.ini &
